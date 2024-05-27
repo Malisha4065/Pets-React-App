3182c06d8e60881fdaac93e5c5ecd22602934229
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-27 21:11:23 +0530

    fetch details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
 const Details = () => {
-    return <h2>Hi!</h2>;
-}
+  const { id } = useParams();
+  const results = useQuery({
+    queryKey: ["details", id],
+    queryFn: fetchPet,
+  });
+
+	if (results.isError) {
+		return <h2>Oh no! Error getting data.</h2>
+	}
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🍥</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} | {pet.breed} | {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Details;
